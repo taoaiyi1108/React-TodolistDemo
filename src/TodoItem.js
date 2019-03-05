f778b5bbea6142163be00a01afedbd7f0fffaa55
wangyongtao <mm18575206484@163.com>
2019-03-05 22:40:12 +0800

    2019年3月5日更新

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -20,11 +20,24 @@ class TodoItem extends Component {
         const { deleteItem, index } = this.props;
         deleteItem(index);
     }
+
+    componentWillReceiveProps() {
+        /* 1、当一个组件从父组件接收了参数
+           2、只要父组件的render函数被重新执行了，子组件的这个生命周期函数就会被执行
+           3、如果这个组件首次从在于父组件中，不会执行
+           4、如果这个组件之前已经从在于父组件中，才会被执行 */
+        console.log("componentWillReceiveProps");
+    }
+
+    componentWillUnmount() {
+        // 组件即将被从页面中剔除的时候执行
+        console.log("componentWillUnmount");
+    }
 }
 
 TodoItem.propTypes = {
     test: PropTypes.string.isRequired,
-    content: PropTypes.arrayOf(PropTypes.number,PropTypes.string),
+    content: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
     deleteItem: PropTypes.func,
     index: PropTypes.number
 }
